ef896cbd202d105af088232d51c4857e102a9af6
ecaqu123 <80189900+ecaqu123@users.noreply.github.com>
2021-09-27 09:44:11 +0700

    Add files via upload

---
+++ Java111.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java;
+
+/**
+ *
+ * @author 62857
+ */
+public class Java11 {
+    public static void main(String[] args) {
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat Mudah Sekali");
+        System.out.println("Dan Sangat Menyenangkan");
+    }
+    
+}
